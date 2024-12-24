2e13f6ebde3190882d2f4fc55f7f98c54eea78b5
Mw313786 <mohammed.23cs074@sode-edu.in>
2024-12-24 13:54:26 +0530

    Update TestThread.java

--- TestThread.java
+++ TestThread.java
@@ -3,7 +3,7 @@ class MyRunnable implements Runnable{
     public void run() {
         try {
             Thread.sleep(5);
-            System.out.println("Thread " +Thread.currentThread().getId() + "running");
+            System.out.println("Thread " +Thread.currentThread().getName() + "running");
         }
         catch(InterruptedException e) {
             e.printStackTrace();
